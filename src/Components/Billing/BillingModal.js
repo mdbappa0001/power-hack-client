777663159517333f1e913c1dcea488cda5f78e78
mdbappa0001 <abutanif366@gmail.com>
2022-06-30 14:32:30 +0600

    modal added

--- src/Components/Billing/BillingModal.js
+++ src/Components/Billing/BillingModal.js
@@ -1,9 +1,54 @@
 import React from 'react';
 
-const BillingModal = () => {
+const BillingModal = ({ add, setAdd }) => {
+
+
+    const handelAddBilling = (e) => {
+        e.preventDefault()
+    }
+
+
     return (
         <div>
-            <h2>This is billing modal</h2>
+            {/* <!-- Put this part before </body> tag --> */}
+            <input type="checkbox" id="add-modal" class="modal-toggle" />
+            <div class="modal">
+                <div class="modal-box rounded">
+                    <h1 className='text-center font-bold text-2xl'>Add / Update Bill</h1>
+                    {/* form start ------------------------------------------------------------------------ */}
+                    <form onSubmit={handelAddBilling} className="py-10">
+                        <div class="relative z-0 w-full mb-6 group">
+                            <input type="email" name="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" />
+                            <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+                        </div>
+                        <div class="relative z-0 w-full mb-6 group">
+                            <input type="text" name="floating_full_name" id="floating_full_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" />
+                            <label for="floating_full_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Full name</label>
+                        </div>
+
+                        <div class="grid xl:grid-cols-2 xl:gap-6">
+                            <div class="relative z-0 w-full mb-6 group">
+                                <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" />
+                                <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (123-456-7890)</label>
+                            </div>
+                            <div class="relative z-0 w-full mb-6 group">
+                                <input type="text" name="floating_amount" id="floating_amount" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" />
+                                <label for="floating_amount" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Paid amount</label>
+                            </div>
+                        </div>
+                        <div className='flex justify-between'>
+                            <button type="submit" class="text-white bg-black hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm w-full sm:w-auto px-5 py-2.5 text-center rounded" onClick={() => setAdd(!add)}>Submit</button>
+                            <button class="text-white bg-black hover:bg-red-400 hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm w-full sm:w-auto px-5 py-2.5 text-center rounded" onClick={() => setAdd(!add)}> Cancel</button>
+                        </div>
+
+                    </form>
+
+
+                    {/* form end ------------------------------------------------------------------------ */}
+
+
+                </div>
+            </div>
         </div>
     );
 };
