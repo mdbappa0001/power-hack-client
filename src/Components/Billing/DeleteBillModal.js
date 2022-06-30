e6c6a2d9c717bd3f4b24cd4826a8b239ad18d25f
mdbappa0001 <abutanif366@gmail.com>
2022-06-30 14:35:46 +0600

    deleteModal added

--- src/Components/Billing/DeleteBillModal.js
+++ src/Components/Billing/DeleteBillModal.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const DeleteBillModal = () => {
+const DeleteBillModal = ({ confirmDelete, setConfirmDelete }) => {
     return (
         <div>
-            <h2>Thsi is delete modal</h2>
+            {/* <!-- Put this part before </body> tag --> */}
+            <input type="checkbox" id="delete-modal" class="modal-toggle" />
+            <div class="modal">
+                <div class="modal-box rounded-none">
+                    <h1 className='text-center text-error font-semibold text-2xl'> Warning!!!</h1>
+                    <button onClick={() => setConfirmDelete(!confirmDelete)}>Close</button>
+                </div>
+            </div>
         </div>
     );
 };
